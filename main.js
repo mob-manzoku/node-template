f0fd8f58dd5f648022f4538eec6f8abe5702bf37
Ryo MANZOKU <manzoku@mobcast.jp>
2014-09-19 16:11:28 +0900

    update

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(process.env + "");
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end(JSON.stringify(process.env));
 }).listen('1080', '0.0.0.0');
 console.log('Server running at http://0.0.0.0:1080');
 
-console.log(process.env);
 
