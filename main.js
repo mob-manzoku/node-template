21f358852491083a320bd1d822bcf4fc8ae3d8ca
Ryo MANZOKU <manzoku@mobcast.jp>
2014-10-08 17:11:38 +0900

    v3

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify(process.env));
+  res.end("v3");
 }).listen('1080', '0.0.0.0');
 console.log('Server running at http://0.0.0.0:1080');
-
-
